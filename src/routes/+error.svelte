497832be5eabb20a585a9a68a483cdd35496ad8c
lalitm1004 <lalitm1004@gmail.com>
2026-01-23 15:22:19 +0530

    feat: create 404 + add analytics

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,7 +1,17 @@
+<script lang="ts">
+    import { Button } from "bits-ui";
+</script>
+
 <main class={`h-dvh w-dvw grid place-items-center`}>
     <div
-        class={`h-[60%] lg:w-[20%] md:w-[50%] w-[80%] grid place-items-center bg-neutral-50 rounded-xl shadow-lg shadow-neutral-950`}
+        class={`h-[60%] lg:w-[20%] md:w-[50%] w-[80%] flex flex-col justify-center items-center gap-4 bg-neutral-50 rounded-xl shadow-lg shadow-neutral-950`}
     >
         <h1 class={`text-4xl`}>404</h1>
+        <Button.Root
+            href={`/`}
+            class={`px-4 py-2 text-sm bg-violet-50 border border-neutral-300 rounded-lg`}
+        >
+            Go Home
+        </Button.Root>
     </div>
 </main>
--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,6 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-// import { SvelteKitPWA, type SvelteKitPWAOptions } from "@vite-pwa/sveltekit";
 
-// const PwaConfig = () => {
-//     let icons = [16, 32, 48, 64, 96, 128, 256, 512].map(size => ({
-//         src: `/assets/logo-${size}.png`,
-//         sizes: `${size}x${size}`,
-//         type: 'image/png'
-//     }))
-
-//     let pwaConfig: SvelteKitPWAOptions = {
-//         registerType: 'autoUpdate',
-//         includeAssets: ['robots.txt'],
-//         manifest: {
-//             name: 'HunQRBox',
-//             short_name: 'HunQRBox',
-//             description: 'bhaiya code daaldo',
-//             theme_color: '#F5F3FF',
-//             background_color: '#3A1593',
-//             display: 'standalone',
-//             start_url: '/',
-//             icons
-//         }
-//     };
-
-//     return SvelteKitPWA(pwaConfig);
-// }
 
 export default defineConfig({ plugins: [tailwindcss(), sveltekit(),] });
-
